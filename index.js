ca412342459a425d194e528a0e654d44e03f385a
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-11-28 15:10:08 +0200

    Modified the way of serving static files

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
+const { startFetchingData, myCache } = require('./dataService/fetchData')
  
 app.use(cors())
  
-const { startFetchingData, myCache } = require('./dataService/fetchData')
-
 const http = require('http')
 const port = process.env.PORT || 3001
 const server = http.createServer(app)
 
-app.use(express.static('./build'))
+app.use(express.static(path.join(__dirname, '/build')))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
