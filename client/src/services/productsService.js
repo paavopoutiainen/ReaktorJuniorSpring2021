95993931ee95a2c12505728e9993d608a77b6b7b
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-11-28 18:26:06 +0200

    Added NODE_ENV check in the productsService for deciding the API uri from which to fetch the product data

--- client/src/services/productsService.js
+++ client/src/services/productsService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseUrl = "https://evening-ridge-26628.herokuapp.com/products"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://evening-ridge-26628.herokuapp.com/products" : "http://localhost:3001/products"
 
 export const fetchProductsData = async () => {
     const response = await axios.get(baseUrl)
